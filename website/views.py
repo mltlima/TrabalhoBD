e0d0707df8f093178e82ef3e7e6365d496694ef1
mltlima <miguellt@dcc.ufrj.br>
2021-10-20 00:01:59 -0300

    Pagina endereço e recursos adicionada

--- website/views.py
+++ website/views.py
@@ -30,7 +30,7 @@ def home():
             ORDER BY `nome` ASC
             """.format(query_extra)
         cursor.execute(sql, query_args)
-        result = cursor.fetchall()
+        result = cursor.fetchall()  
 
     return render_template("home.html", escola=result, search=request.args.get("search"), user=current_user)
 
@@ -49,6 +49,31 @@ def delete_note():
 
 
 @views.route("/escola")
+def escola():
+    with connection.cursor() as cursor:
+        query_args = []
+        query_extra = ""
+        if request.args.get("search"):
+            query_extra = """
+                WHERE LOWER(`nome`) LIKE LOWER(%s)
+            """
+            search = "%{}%".format(request.args["search"])
+            query_args = [search]
+        sql = """
+            SELECT
+                `escola`.*,
+                `Telefone`.`Telefone` as telefone
+            FROM `escola`
+            INNER JOIN `telefone` ON `escola`.`Código` = `telefone`.`fk_Escola_Código`
+            {}
+            ORDER BY `nome` ASC
+            """.format(query_extra)
+        cursor.execute(sql, query_args)
+        result = cursor.fetchall()
+    return render_template("escola.html", escola=result, search=request.args.get("search"), user=current_user)
+
+
+@views.route("/endereco")
 def endereco():
     with connection.cursor() as cursor:
         query_args = []
@@ -62,7 +87,7 @@ def endereco():
         sql = """
             SELECT
                 `escola`.*,
-                `endereço`.`distrito` as distrito
+                `endereço`.*
             FROM `escola`
             INNER JOIN `endereço` ON `escola`.`fk_cep` = `endereço`.`cep`
             {}
@@ -70,4 +95,30 @@ def endereco():
             """.format(query_extra)
         cursor.execute(sql, query_args)
         result = cursor.fetchall()
-    return render_template("escola.html", escola=result, search=request.args.get("search"), user=current_user)
+    return render_template("endereco.html", endereco=result, search=request.args.get("search"), user=current_user)
+
+
+@views.route("/recurso")
+def recurso():
+    with connection.cursor() as cursor:
+        query_args = []
+        query_extra = ""
+        if request.args.get("search"):
+            query_extra = """
+                WHERE LOWER(`nome`) LIKE LOWER(%s)
+            """
+            search = "%{}%".format(request.args["search"])
+            query_args = [search]
+        sql = """
+            SELECT
+                `escola`.`nome` as `NomeEscola`,
+                `Recurso`.*
+            FROM `escola`
+            INNER JOIN `Escola_Recurso` ON `Escola_Recurso`.`fk_Escola_Código` = `escola`.`código`
+            INNER JOIN `Recurso` ON `Recurso`.`ID` = `Escola_Recurso`.`fk_Recurso_ID`
+            {}
+            ORDER BY `NomeEscola` ASC
+            """.format(query_extra)
+        cursor.execute(sql, query_args)
+        result = cursor.fetchall()
+    return render_template("recurso.html", recurso=result, search=request.args.get("search"), user=current_user)
